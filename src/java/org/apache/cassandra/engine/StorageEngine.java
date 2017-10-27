68c0fe2526034ad21216454a6ccd97a0db7124b6
Dikang Gu <dikang85@gmail.com>
2017-10-27 12:14:28 -0700

    remove dependency of CFS in storage engine API

--- src/java/org/apache/cassandra/engine/StorageEngine.java
+++ src/java/org/apache/cassandra/engine/StorageEngine.java
@@ -19,49 +19,108 @@
 package org.apache.cassandra.engine;
 
 import java.util.Collection;
-import java.util.UUID;
 
-import org.apache.cassandra.db.ColumnFamilyStore;
-import org.apache.cassandra.db.SinglePartitionReadCommand;
+import org.apache.cassandra.db.Clustering;
+import org.apache.cassandra.db.DecoratedKey;
+import org.apache.cassandra.db.DeletionTime;
+import org.apache.cassandra.db.RegularAndStaticColumns;
+import org.apache.cassandra.db.Slices;
+import org.apache.cassandra.db.filter.ColumnFilter;
 import org.apache.cassandra.db.partitions.PartitionUpdate;
+import org.apache.cassandra.db.rows.EncodingStats;
+import org.apache.cassandra.db.rows.Row;
 import org.apache.cassandra.db.rows.UnfilteredRowIterator;
 import org.apache.cassandra.dht.Range;
 import org.apache.cassandra.dht.Token;
 import org.apache.cassandra.engine.streaming.AbstractStreamReceiveTask;
 import org.apache.cassandra.engine.streaming.AbstractStreamTransferTask;
+import org.apache.cassandra.schema.TableMetadata;
 import org.apache.cassandra.streaming.StreamSession;
 import org.apache.cassandra.streaming.StreamSummary;
+import org.apache.cassandra.utils.SearchIterator;
 
 public interface StorageEngine
 {
-    void openColumnFamilyStore(ColumnFamilyStore cfs);
+    /**
+     * This method should create all necessary files but does not need to open the table.
+     */
+    void create(String tableName);
 
-    void apply(final ColumnFamilyStore cfs,
-               final PartitionUpdate partitionUpdate,
-               final boolean writeCommitLog);
+    /**
+     * Before any read or write operations are performaned on a table, the cassandra server
+     * will call the open method to open the table data.
+     */
+    void open(String tableName);
 
-    UnfilteredRowIterator queryStorage(ColumnFamilyStore cfs,
-                                       SinglePartitionReadCommand readCommand);
+    /**
+     * Apply a mutation to storage engine
+     */
+    void applyMutate(String tableName,
+                     PartitionUpdate partitionUpdate,
+                     boolean writeCommitLog);
 
-    void forceFlush(final ColumnFamilyStore cfs);
+    /**
+     * In-memory representation of a Partition.
+     *
+     * Storage engine needs to implement iterators (UnfilteredRowIterator) to
+     * avoid "materializing" a full partition/query response in memory as much as possible.
+     */
+    public interface Partition
+    {
+        public TableMetadata metadata();
+        public DecoratedKey partitionKey();
+        public DeletionTime partitionLevelDeletion();
 
-    void truncate(final ColumnFamilyStore cfs);
+        public RegularAndStaticColumns columns();
 
-    void close(final ColumnFamilyStore cfs);
+        public EncodingStats stats();
+
+        /**
+         * Whether the partition object has no informations at all, including any deletion informations.
+         */
+        public boolean isEmpty();
+
+        /**
+         * Returns the row corresponding to the provided clustering, or null if there is not such row.
+         */
+        public Row getRow(Clustering clustering);
+
+        /**
+         * Returns an iterator that allows to search specific rows efficiently.
+         */
+        public SearchIterator<Clustering, Row> searchIterator(ColumnFilter columns, boolean reversed);
+
+        /**
+         * Returns an UnfilteredRowIterator over all the rows/RT contained by this partition.
+         */
+        public UnfilteredRowIterator unfilteredIterator();
+
+        /**
+         * Returns an UnfilteredRowIterator over the rows/RT contained by this partition
+         * selected by the provided slices.
+         */
+        public UnfilteredRowIterator unfilteredIterator(ColumnFilter columns, Slices slices, boolean reversed);
+    }
+
+    void forceFlush(String tableName);
+
+    void truncate(String tableName);
+
+    void close(String tableName);
 
     void setCompactionThroughputMbPerSec(int throughputMbPerSec);
 
-    void forceMajorCompaction(ColumnFamilyStore cfs);
+    void forceMajorCompaction(String tableName);
 
     /**
      *  Streaming APIs
      */
     AbstractStreamTransferTask getStreamTransferTask(StreamSession session,
-                                                     UUID cfId,
+                                                     String tableName,
                                                      Collection<Range<Token>> ranges);
 
     AbstractStreamReceiveTask getStreamReceiveTask(StreamSession session,
                                                    StreamSummary summary);
 
-    boolean cleanUpRanges(final ColumnFamilyStore cfs);
+    boolean cleanUpRanges(final String tableName);
 }
